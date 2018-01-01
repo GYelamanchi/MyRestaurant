862da8da59a62254ad8713a6aa3054d9a6fa6aca
GYelamanchi <gani.yalamanchi@gmail.com>
2018-01-01 20:31:46 +0000

    Initial commit

---
+++ src/app/mock-items.ts
@@ -0,0 +1,8 @@
+import {Item} from './item';
+
+export const ITEMS: Item[] = [
+    { id: 1, name: 'X' , category: 'sweet'},
+    { id: 2, name: 'Y',  category: 'apetizer'},
+    { id: 3, name: 'Z', category: 'soup'}
+    
+  ];
